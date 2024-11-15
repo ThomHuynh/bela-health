105082cb40f02444cde5f57a8cca3485529f9cc1
Thom Huynh <me@thomhuynh.com>
2024-11-15 12:36:33 +0200

    anonymous coming soon page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bela-health.de',
-    base: 'bela-health',
+  site: 'https://bela-health.de',
+  base: 'bela-health',
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  })],
 });
